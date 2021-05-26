48bd1692f1d4e85415a8c013f2aae3b98e99da1d
Tine Kondo <kondotine@gmail.com>
2021-05-26 23:45:17 +0200

    chore(all): move files to root

--- ngx-cookieconsent/libs/ngx-cookieconsent/src/lib/event/initialize.event.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Event fired when Cookie Consent initializes.
- */
-export interface NgcInitializeEvent {
-  /**
-   * The status of cookie consent
-   */
-  status: 'allow' | 'deny' | 'dismiss';
-}
