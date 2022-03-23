c398496a7031b8ed128e9407eb9b825ef356d356
Jaco ter Braak <jaco.ter.braak@entrnce.com>
2022-03-23 09:48:20 +0100

    feat(core): migrate to Angular 13 and Nx 13

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
+const { getJestProjects } = require('@nrwl/jest');
+
 module.exports = {
   projects: [
+    ...getJestProjects(),
     '<rootDir>/apps/ngx-cookieconsent-demo',
     '<rootDir>/libs/ngx-cookieconsent',
   ],
