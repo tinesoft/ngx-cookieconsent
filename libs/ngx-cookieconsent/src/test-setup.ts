7ccf807f70bb43348aa907ef43798f690e6267cc
Tine Kondo <kondotine@gmail.com>
2022-10-09 08:33:30 +0000

    fix(core): remove wrong peerDependency on `platform-browser-dynamic`

--- libs/ngx-cookieconsent/src/test-setup.ts
+++ libs/ngx-cookieconsent/src/test-setup.ts
@@ -2,16 +2,3 @@ import 'jest-preset-angular/setup-jest';
 
 import 'cookieconsent/build/cookieconsent.min.js';
 //import 'cookieconsent/build/cookieconsent.min.css';
-
-import { getTestBed } from '@angular/core/testing';
-import {
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting,
-} from '@angular/platform-browser-dynamic/testing';
-
-getTestBed().resetTestEnvironment();
-getTestBed().initTestEnvironment(
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting(),
-  { teardown: { destroyAfterEach: false } }
-);
