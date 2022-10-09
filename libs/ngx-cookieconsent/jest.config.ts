440f54f1028c66a87e2fdf9c6f4b625192ea15a8
Tine Kondo <kondotine@gmail.com>
2022-10-09 07:27:32 +0000

    feat(core): migrate to Angular 14 and Nx 14
    BREAKING CHANGE: Angular 14 is now the minimal required version
    
    Closes #123

--- libs/ngx-cookieconsent/jest.config.ts
+++ libs/ngx-cookieconsent/jest.config.ts
@@ -1,5 +1,6 @@
-module.exports = {
-  displayName: 'ngx-cookieconsent-demo',
+/* eslint-disable */
+export default {
+  displayName: 'ngx-cookieconsent',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -8,7 +9,7 @@ module.exports = {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/apps/ngx-cookieconsent-demo',
+  coverageDirectory: '../../coverage/libs/ngx-cookieconsent',
   snapshotSerializers: [
     'jest-preset-angular/build/serializers/no-ng-attributes',
     'jest-preset-angular/build/serializers/ng-snapshot',
